52842be36a967e6e386b516175020c9f64c24b02
Shehab Elden Moselhy <141828105+Shehab-Moselhy@users.noreply.github.com>
2023-11-27 18:16:01 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 21, 2023
+ *      Author: Shehab Eldin Saber
+ */
+
+#include "../include/LIB/STD_TYPES.h"
+#include "../include/LIB/BIT_MATH.h"
+
+#include "../include/MCAL/DIO_Interface.h"
+#include "../include/HAL/LED/LED_Interface.h"
+#define F_CPU 8000000UL
+
+#define PRESSED 0
+
+int main(void)
+{
+	MDIO_voidInit();
+	while(1)
+	{
+		HSSD_voidDisplayAscending(PORTD);
+	}
+}
